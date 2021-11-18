5cf094b46845fbdc0986a68c11e24a6f2cc9695e
Flávio Arêas <vflavio9@gmail.com>
2021-11-17 21:33:00 -0300

    chore: connect to mongodb

---
+++ src/database/MongoConnection.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const mongoURL = process.env.MONGO_CONNECTION || '';
+
+class MongoConnection {
+  constructor() {}
+
+  public async connect(): Promise<void> {
+    try {
+      await mongoose.connect(mongoURL, {
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+      });
+
+      console.log('Database connected');
+    } catch (err: any) {
+      console.error(err.message);
+      process.exit(1);
+    }
+  }
+}
+
+export { MongoConnection };
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import 'dotenv/config';
 import express from 'express';
 
+import { MongoConnection } from './database/MongoConnection';
 import { UrlController } from './controllers/UrlController';
 
 const port = process.env.PORT || 3333;
 
 const server = express();
-
-const urlController = new UrlController();
-
 server.use(express.json());
 
+const database = new MongoConnection();
+database.connect();
+
+const urlController = new UrlController();
 server.post('/shortener', urlController.shortener);
 server.get('/:hash', urlController.redirect);
 
